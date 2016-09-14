bb023e5c9637c03e9aad5c295a71c7a6626f680c
Steven Burris <stevenburris.jr@gmail.com>
2016-09-14 14:39:49 -0400

    entered options

--- src/ATM.java
+++ src/ATM.java
@@ -19,6 +19,16 @@ public class ATM {
             throw new Exception("Your name is needed");
         }
         System.out.println("Welcome, "+ name);
+        System.lineSeparator();
+        System.out.println("Please enter the number of the operation you would like to perform.");
+        System.lineSeparator();
+        System.out.println("1: Check Balance");
+        System.lineSeparator();
+        System.out.println("2: Withdraw Funds");
+        System.lineSeparator();
+        System.out.println("3: Cancel");
+
+
 
 
 
