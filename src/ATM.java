dbfc30ff27a02ae5d808ebb8c95038840f762cbc
Steven Burris <stevenburris.jr@gmail.com>
2016-09-14 14:28:48 -0400

    Enter Name

--- src/ATM.java
+++ src/ATM.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 /**
  * Created by stevenburris on 9/14/16.
  */
 public class ATM {
+
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) throws Exception{
+
+        String name;
+
+        System.out.println("Please enter your name.");
+        name = ATM.scanner.nextLine();
+        if(name.isEmpty()){
+            throw new Exception("Your name is needed");
+        }
+        System.out.println("Welcome, "+ name);
+
+
+
+
+    }
 }
