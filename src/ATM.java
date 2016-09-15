6bf3f0358472b2db494968987cc8b161bdce7472
Steven Burris <stevenburris.jr@gmail.com>
2016-09-14 21:14:57 -0400

    changed loop

--- src/ATM.java
+++ src/ATM.java
@@ -7,19 +7,21 @@ public class ATM {
 
 
     static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner2 = new Scanner(System.in);
     public static boolean newTransaction;
 
     public static void main(String[] args) throws Exception{
 
         boolean newTransaction = true;
+        String answers;
 
         while(newTransaction){
             Person newPerson = new Person();
             newPerson.enterName();
             newPerson.chooseOption();
             System.out.println("Enter 1 for another transaction or 2 to exit.");
-            String answers = scanner.nextLine();
-            if(answers.equalsIgnoreCase("2")){
+            answers = scanner2.nextLine();
+            if(answers.equals("2")){
                 newTransaction = false;
             }
         }
