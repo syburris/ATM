ff293124beaa2e37c74c1d9e8113c062fe68b3ad
Steven Burris <stevenburris.jr@gmail.com>
2016-09-16 14:47:42 -0400

    Fixed the new transaction bug

---
+++ src/ATMTakeTwo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+/**
+ * Created by stevenburris on 9/16/16.
+ */
+public class ATMTakeTwo {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static boolean newTransaction = true;
+}
---
+++ src/HopefullyUserName.java
@@ -0,0 +1,8 @@
+/**
+ * Created by stevenburris on 9/16/16.
+ */
+public class HopefullyUserName {
+    String userName;
+    String password;
+    double balance;
+}
